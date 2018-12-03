38202d0daf9185dbeade099a36ea0706f0b74958
Andrew Thal <athal7@me.com>
2018-12-03 10:11:36 -0500

    Fix logging and interval of slack status

--- .set-meeting-status.sh
+++ .set-meeting-status.sh
@@ -14,16 +14,17 @@ end tell
 
 eventsNow=$(/usr/local/bin/icalBuddy -ic athal@2u.com -ea eventsNow)
 statusFile="/tmp/event-now"
+now=$(date +%d.%m.%y-%H:%M:%S)
 
 if [[ $eventsNow ]]; then
-  echo "In a meeting"
+  echo "$now In a meeting"
   if [ ! -e "$statusFile" ]; then
     echo "Setting status"
     osascript -e $script
     touch $statusFile
   fi
 else
-  echo "Free"
+  echo "$now Free"
   if [ -e "$statusFile" ]; then
     rm $statusFile
   fi
