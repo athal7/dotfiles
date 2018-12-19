f15b0c22f0cb0c81075474361a477b2072ee1106
Andrew Thal <andrewthal88@gmail.com>
2018-12-19 11:16:44 -0500

    Meeting status script can clear too

--- .set-meeting-status.sh
+++ .set-meeting-status.sh
@@ -1,16 +1,18 @@
 #!/bin/zsh
 
-script="
-display dialog \"Set Slack meeting status?\"
+function setStatus {
+  osascript -e "
+display dialog \"Set Slack Status?\"
 
 activate application \"Slack\"
 
 tell application \"System Events\"
-	set textToType to \"/status :meeting:\"
+	set textToType to \"/status $1\"
 	keystroke textToType
 	keystroke return
 end tell
 "
+}
 
 eventsNow=$(/usr/local/bin/icalBuddy -ic athal@2u.com -ea eventsNow)
 statusFile="/tmp/event-now"
@@ -20,12 +22,13 @@ if [[ $eventsNow ]]; then
   echo "$now In a meeting"
   if [ ! -e "$statusFile" ]; then
     echo "Setting status"
-    osascript -e $script
+    setStatus ":meeting:"
     touch $statusFile
   fi
 else
   echo "$now Free"
   if [ -e "$statusFile" ]; then
+    setStatus "clear"
     rm $statusFile
   fi
 fi
