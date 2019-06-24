a9a8e062f8e7c712c892ae99853123a30bc29191
Andrew Thal <andrewthal88@gmail.com>
2019-06-24 10:02:55 -0400

    vscode extension cleanup

--- .vscode/extensions/ivangabriele.vscode-git-add-and-commit-2.0.4/out/helpers/replaceStringWith.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function replaceStringWith(str, patterns) {
-    return patterns.reduce((reducedStr, { pattern, with: val }) => {
-        return reducedStr.replace(pattern, val);
-    }, str);
-}
-exports.default = replaceStringWith;
--- .vscode/extensions/ivangabriele.vscode-git-add-and-commit-2.0.4/out/helpers/validateCommitMessage.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function validateCommitMessage(message) {
-    return message !== undefined && message !== null && message.trim() !== '';
-}
-exports.default = validateCommitMessage;
