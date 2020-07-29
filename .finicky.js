890e4e3d84b8c62e0aac115970899362dc256b49
Andrew Thal <andrewthal88@gmail.com>
2020-07-29 10:57:45 -0400

    Chrome just for meet

--- .finicky.js
+++ .finicky.js
@@ -1,21 +1,9 @@
 module.exports = {
-  defaultBrowser: "Safari",
-  handlers: [
-    {
-      match: finicky.matchHostnames([
-        /github/, 
-        /google/,
-        /slack/,
-        /onelogin/,
-        /2u/,
-        /fellow/,
-        /zoom/,
-        /lucidchart/,
-        /imsglobal/,
-        /localhost/,
-        /127.0.0.1/,
-      ]),
-      browser: "Google Chrome"
-    }
-  ]
-};
+    defaultBrowser: 'Safari',
+    handlers: [
+        {
+            match: finicky.matchHostnames([/meet.google/]),
+            browser: 'Google Chrome',
+        },
+    ],
+}
