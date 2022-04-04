769c6275ccf550e05b13bd6c67b188098b44e348
Andrew Thal <467872+athal7@users.noreply.github.com>
2022-04-04 14:16:54 -0400

    Use scripts

--- setup.sh
+++ setup.sh
@@ -2,9 +2,5 @@
 
 ## Setup hook for use with Github Codespaces 
 ## https://docs.github.com/en/codespaces/setting-up-your-codespace/personalizing-codespaces-for-your-account
-make symlink
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/codespace/.profile
-source ~/.profile
-brew install bat git-delta eslint navi prettier
-source ~/.bashrc
+
+sh -c "$(curl -fsLS chezmoi.io/get)" -- init --apply $GITHUB_USERNAME
