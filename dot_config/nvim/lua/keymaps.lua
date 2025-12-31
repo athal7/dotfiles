bafb7aa67b5c7af23ffe8c047935adf6dcde94f8
Andrew Thal <467872+athal7@users.noreply.github.com>
2025-12-30 20:52:21 -0600

    feat: add neovim/kitty/autojump, fix shell setup, load .env in zshrc
    - Add kitty terminal, neovim, and autojump to Brewfile
    - Fix set-shell script ($whoami → $(whoami))
    - Source ~/.env with auto-export in zshrc
    - Change default editor to nvim
    - Add neovim config with lazy.nvim, LSP, treesitter

---
+++ dot_config/nvim/lua/keymaps.lua
@@ -0,0 +1,42 @@
+local map = vim.keymap.set
+
+-- Clear search highlight
+map("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Window navigation
+map("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
+map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
+map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
+
+-- Resize windows
+map("n", "<C-Up>", "<cmd>resize +2<CR>", { desc = "Increase height" })
+map("n", "<C-Down>", "<cmd>resize -2<CR>", { desc = "Decrease height" })
+map("n", "<C-Left>", "<cmd>vertical resize -2<CR>", { desc = "Decrease width" })
+map("n", "<C-Right>", "<cmd>vertical resize +2<CR>", { desc = "Increase width" })
+
+-- Buffer navigation
+map("n", "<S-h>", "<cmd>bprevious<CR>", { desc = "Prev buffer" })
+map("n", "<S-l>", "<cmd>bnext<CR>", { desc = "Next buffer" })
+map("n", "<leader>bd", "<cmd>bdelete<CR>", { desc = "Delete buffer" })
+
+-- Move lines
+map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move down" })
+map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move up" })
+
+-- Stay centered
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+
+-- Better paste (don't yank replaced text)
+map("x", "<leader>p", [["_dP]], { desc = "Paste without yank" })
+
+-- Quick save
+map("n", "<leader>w", "<cmd>w<CR>", { desc = "Save" })
+
+-- Diagnostic navigation
+map("n", "[d", vim.diagnostic.goto_prev, { desc = "Prev diagnostic" })
+map("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
+map("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic" })
---
+++ dot_config/nvim/lua/options.lua
@@ -0,0 +1,46 @@
+local opt = vim.opt
+
+-- Line numbers
+opt.number = true
+opt.relativenumber = true
+
+-- Tabs & indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+opt.smartindent = true
+
+-- Line wrapping
+opt.wrap = false
+
+-- Search
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = true
+opt.incsearch = true
+
+-- Appearance
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+opt.cursorline = true
+opt.scrolloff = 8
+opt.sidescrolloff = 8
+
+-- Behavior
+opt.hidden = true
+opt.mouse = "a"
+opt.clipboard = "unnamedplus"
+opt.splitbelow = true
+opt.splitright = true
+opt.timeoutlen = 300
+opt.updatetime = 250
+
+-- Undo & backup
+opt.undofile = true
+opt.swapfile = false
+opt.backup = false
+
+-- Completion
+opt.completeopt = "menuone,noselect"
